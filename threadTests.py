68ddb20553d6f6b00f0c607a7af8f3b7cc894a57
DNIIBOY <daniel.nettelfield@gmail.com>
2022-11-14 19:58:24 +0100

    final testing of threading before going GUI

--- threadTests.py
+++ threadTests.py
@@ -1,19 +1,45 @@
 import threading
+import multiprocessing
 import time
+import sys
 
-response = None
-
-
-def get_response():
-    global response
-    response = input("Do you wish to reconnect? ")
-    print('As you wish')
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import parse_qs, urlparse
+import json
 
 
-thread = threading.Thread(target=get_response, daemon=True)
-thread.start()
-time.sleep(4)
-thread.join(2)
+class RequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        parsed_path = urlparse(self.path)
+        params = parse_qs(parsed_path.query)
+        self.send_response(200)
+        self.end_headers()
+        process.kill()
 
-if response is None:
-    print('Exiting')
+        self.wfile.write(json.dumps({
+            'method': self.command,
+            'params': params,
+            'request_version': self.request_version,
+            'protocol_version': self.protocol_version,
+        }).encode())
+        return
+
+
+def test_function():
+    while True:
+        print("test")
+        time.sleep(1)
+
+
+process = multiprocessing.Process(target=test_function)
+
+if __name__ == '__main__':
+    server = HTTPServer(('localhost', 8080), RequestHandler)
+    server_thread = threading.Thread(target=server.serve_forever)
+    server_thread.daemon = True
+    server_thread.start()
+    print('Server loop running in thread:', server_thread.name)
+    process.start()
+
+    while True:
+        time.sleep(1)
